cb1e4276bfd89f696c6781f809d2cbff4a8100ab
Robert Maina <robert.maina@student.moringaschool.com>
2021-07-25 02:14:59 +0300

    used a form and added a httpclientmodule

--- Moringa_Core/Github-Search/src/app/search-service.service.ts
+++ Moringa_Core/Github-Search/src/app/search-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchServiceService {
 
-  constructor() { }
+  names: any[] = [];
+
+  constructor(private http:HttpClient) { }
+
+  getName() {
+    return this.http.get('https://api.github.com/users/Robert-Moringa?access_token=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').subscribe((response: any) => {
+      this.names=response.data;
+    })
+  }
 }
--- Moringa_Core/Github-Search/src/app/search/search.component.ts
+++ Moringa_Core/Github-Search/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchServiceService } from '../search-service.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService: SearchServiceService) { }
 
   ngOnInit(): void {
+    this.searchService.getName();
   }
 
 }
