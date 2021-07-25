49f19d7554fca48b5748a2354f6449aa4c65b1ee
Robert Maina <robert.maina@student.moringaschool.com>
2021-07-25 05:49:32 +0300

    added a directive

--- Moringa_Core/Github-Search/src/app/app.module.ts
+++ Moringa_Core/Github-Search/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
